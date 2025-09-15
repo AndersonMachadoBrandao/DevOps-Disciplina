7e2f29e3030d996f14dd116fc5d5e802882918bb
AndersonMachadoBrandao <anderlinnn765@gmail.com>
2025-09-15 18:45:32 -0300

    Adicionando interface de escolha

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24476d28a963b06a8d6462e8afd671c4664875ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c884a5f777a13bc17dc582696dcd1104d121f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
