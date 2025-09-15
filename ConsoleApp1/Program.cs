63e21abb365fb3e06eca18f25edcb32b50856e1d
AndersonMachadoBrandao <anderlinnn765@gmail.com>
2025-09-15 18:35:10 -0300

    Primeiro commit, inserindo função de soma
    Primeiro commit, inserindo função de soma

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita dois números ao usuário
+        Console.Write("Digite o primeiro número: ");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        // Faz a soma
+        int resultado = numero1 + numero2;
+
+        // Exibe o resultado
+        Console.WriteLine("A soma é: " + resultado);
+    }
+}
